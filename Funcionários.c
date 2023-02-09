538c7f43eada6070b71b812aeaa5272fd79621dd
Vladimyr Guedes <123104597+SrVlady@users.noreply.github.com>
2023-02-09 15:16:42 -0300

    Create Funcionários.c

--- Funcionários.c
+++ Funcionários.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct funcionario
-{
+struct funcionario {
     char nome[50], cargo[50];
     float salario;
     int identificador;
 };
 
+void imprime(struct funcionario *f, int quant_funcionarios) {
+    int i;
+
+    for(i = 0; i < quant_funcionarios; i++) {
+        
+        printf("\n-------------------------\n");
+        printf("Funcionario num.%d", i + 1);
+        printf("\nNome: %s", f[i].nome);
+        printf("\nCargo: %s", f[i].cargo);
+        printf("\nSalario: R$%.2f", f[i].salario);
+        printf("\nIdentificador: %d", f[i].identificador);
+    }
+
+}
+
+void altera_salario(struct funcionario *f) {
+    int num_f;
+
+    printf("\nDigite o numero do funcionario que deseja alterar o salario: ");
+    scanf("%d", &num_f);
+    printf("\nDigite o novo salario do funcionario num.%d: ", num_f);
+    scanf("%f", &f[num_f - 1].salario);
+}
+
 int main(void) {
+    char escolha;
 
     struct funcionario *f = (struct funcionario *)malloc(sizeof(struct funcionario));
 
@@ -33,14 +57,18 @@ int main(void) {
         scanf("%d", &f[i].identificador);
     }
 
-    for(i = 0; i < quant_funcionarios; i++) {
-        
-        printf("\n-------------------------\n");
-        printf("Funcionario num. %d", i + 1);
-        printf("\nNome: %s", f[i].nome);
-        printf("\nCargo: %s", f[i].cargo);
-        printf("\nSalario: %.2f", f[i].salario);
-        printf("\nIdentificador: %d", f[i].identificador);
+    imprime(f, quant_funcionarios);
+
+    printf("\n\nDeseja alterar o salario de algum funcionario?\nDigite 's' para sim, e 'n' para nao: ");
+    scanf(" %c", &escolha);
+
+    if(escolha == 's') {
+        altera_salario(f);
+        imprime(f, quant_funcionarios);
+    }
+
+    else {
+        printf("\nFim do programa!");
     }
 
     free(f);
